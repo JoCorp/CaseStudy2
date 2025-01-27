e3467b3032ac1cad270b47385465abe5d2e3bba7
Nan-ge64 <ge64beh@mytum.de>
2025-01-27 18:06:35 +0100

    UPDATED MU_DETERM for including dt instead of t

--- mu_determination.m
+++ mu_determination.m
@@ -8,8 +8,8 @@ function [mu] = mu_determination(t,x)
 
     % calculate μ from slope
 for i = 1:length(x)-1
-    for j = 1:length(t)
-    mu(i+1) = (ln_x(i+1) - ln_x(i)) / t(j);
+    for j = 1:length(t)-1
+    mu(i+1) = (ln_x(i+1) - ln_x(i)) / (t(j+1) - t(j));
     end
 end
 end
